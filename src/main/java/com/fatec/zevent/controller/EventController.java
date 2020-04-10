d94d1225e5032d650ec3240dd701098c18ef06c6
Gabanelli <gabanelli.guilherme@gmail.com>
2020-04-10 01:31:37 -0300

    Mudança de configurações

--- src/main/java/com/fatec/zevent/controller/EventController.java
+++ src/main/java/com/fatec/zevent/controller/EventController.java
@@ -9,6 +9,8 @@ public class EventController {
 
     public List<Event> getAllPublicEvents() {
         Event event = new Event();
+        event.setName("Google Cloud");
+        event.setDesc("Evento da google para realizar festa de TI");
         List<Event> events = new ArrayList<>();
         events.add(event);
         return events;
