12dbd3b3a9c243232bde1b081de9ccad8196f7f1
Gabanelli <gabanelli.guilherme@gmail.com>
2020-06-28 20:21:27 -0300

    Alteração eventos para front

--- src/main/java/com/fatec/zevent/model/Evento.java
+++ src/main/java/com/fatec/zevent/model/Evento.java
@@ -1,12 +1,9 @@
 package com.fatec.zevent.model;
 
-import com.fatec.zevent.model.enumeration.CategoryEnum;
-import com.fatec.zevent.model.enumeration.EventTypeEnum;
 import lombok.*;
 
 import javax.persistence.*;
 
-// @Document(collection = "events")
 @Data
 @Getter
 @Setter
@@ -20,36 +17,27 @@ public class Evento implements Checkable {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private long id;
 
-    //@NotNull(message = "Event's name must not be null")
     @Column(name = "name")
     private String name;
 
-    //@NotNull(message = "Event's category must not be null")
     @Column(name = "category")
     private String category;
 
-    //@NotNull(message = "Event's type must not be null")
     @Column(name = "type")
     private String type;
 
-    //@NotNull(message = "Event's desc must not be null")
     @Column(name="`desc`")
     private String desc;
 
-    //@NotNull(message = "Event's initDate must not be null")
     @Column(name = "init_date")
     private String initDate;
 
-    //@NotNull(message = "Event's finishDate must not be null")
     @Column(name = "finish_date")
     private String finishDate;
 
-    //@NotNull(message = "Event's value must not be null")
     @Column(name = "value")
     private int value;
 
-    //@NotNull(message = "Event's seats must not be null")
-    //@Range(min = 1)
     @Column(name = "seats")
     private int seats;
 
