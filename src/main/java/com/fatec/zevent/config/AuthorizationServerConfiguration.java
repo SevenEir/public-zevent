8bec4e0b7e86391434fc92f3b12f7d65f8b98231
Gabanelli <gabanelli.guilherme@gmail.com>
2020-06-28 21:58:54 -0300

    Correção autenticação

--- src/main/java/com/fatec/zevent/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/fatec/zevent/config/AuthorizationServerConfiguration.java
@@ -3,15 +3,19 @@ package com.fatec.zevent.config;
 import com.fatec.zevent.service.IUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
+@Configuration
+@EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
@@ -31,8 +35,8 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("rokin-client")
-                .secret(passwordEncoder.encode("secret"))
+                .withClient("zevent-client")
+                .secret(passwordEncoder.encode("123"))
                 .authorizedGrantTypes("password", "client_credentials", "refresh_token")
                 .scopes("all")
                 .accessTokenValiditySeconds(3600)
--- src/main/java/com/fatec/zevent/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/fatec/zevent/service/impl/UserDetailsServiceImpl.java
@@ -9,10 +9,12 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+@Service
 public class UserDetailsServiceImpl implements IUserDetailsService {
 
     @Autowired
