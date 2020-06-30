6b6631159ad24751726f3dfce6c446f393509603
Roberto <Almeida>
2020-06-29 22:20:30 -0300

    Chamada de proc

--- src/main/java/com/fatec/zevent/DAO/EventDAO.java
+++ src/main/java/com/fatec/zevent/DAO/EventDAO.java
@@ -1,6 +1,9 @@
 package com.fatec.zevent.DAO;
 
 import com.fatec.zevent.model.Evento;
+
+import antlr.debug.Event;
+
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -39,5 +42,15 @@ public interface EventDAO extends CrudRepository<Evento, Integer> {
             nativeQuery = true
     )
     List<Evento> findAllPublicEvents();
+    
+    @Transactional(readOnly = false, isolation = Isolation.DEFAULT, propagation = Propagation.REQUIRED, rollbackFor = {Exception.class})
+    @Modifying
+    @Query(
+    		value = "CALL `zevent`.`INSERE_NOVO_EVENTO`(?, ?, ?, ?, ?, ?, ?, ?);"
+    		
+    		)
+    void insertEvent(String name, String category, String type, String desc, int value, int seats, 
+    		String finishDate, String initDate);
+    
 
 }
