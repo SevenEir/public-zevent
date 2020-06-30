11495eee1c44a030069a8092525650612b83a384
Gabanelli <gabanelli.guilherme@gmail.com>
2020-06-30 19:23:00 -0300

    Role add fixed

--- src/main/java/com/fatec/zevent/model/Role.java
+++ src/main/java/com/fatec/zevent/model/Role.java
@@ -15,6 +15,13 @@ public class Role implements GrantedAuthority {
     @Override
     public String getAuthority() {
         return  this.name;
-}
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
